72f1a919ab4eee9d0f9ec26ff59ba8027506ea19
infinite-disorder <cyrill.sitnikov@gmail.com>
2017-10-01 14:59:09 +0300

    добавлены структуры для графа и поиска кратчайшего пути на наем

--- gene_slider.py
+++ gene_slider.py
@@ -1,30 +1,54 @@
 # gene_slider.py
 
-POINTS_FOR_MATCH = 5
 
-def count_match_points(a, b):
-    if a == b:
-        return POINTS_FOR_MATCH
-    return 0    
+def match_points(a, b):
+    if (a == b):
+        return 1
+    return 0     
 
-def build_match_matrix(A, B):
-    matrix = []
-    a_len = len(A)
-    b_len = len(B)
-    i = 0
-    while i < a_len:
-        matrix.append([])
-        j = 0
-        while j < b_len:
-            matrix[i].append((A[i], B[j],count_match_points(A[i], B[j])))
-            j += 1
-        i += 1    
-    return matrix
+class Edge:
+    def __init__(self, v, weigth):
+        self.v = v
+        self.weigth = weigth
+
+class Vertex:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+        self.edges = []
+
+    def add_edge(e):
+        self.edges.append(e)
+        return self    
+
+class Graph:
+    def __init__(self, A, B):
+        self.A = A
+        self.B = B
+        self.vertices = {}
+        i = 0
+        while i < len(A):
+            j = 0
+            while j < len(B):
+                self.vertices[(i, j)] = Vertex(i, j)  
+                j += 1
+            i += 1     
+
+        while i < len(A):
+            j = 0
+            while j < len(B):
+                if (i < len(A) - 1):
+                    self.vertces[(i, j)].add_edge(Edge(self.vertices[(i + 1, j)], match_points(A[i], B[j])))
+                    if (j < len(B) - 1):
+                        self.vertces[(i, j)].add_edge(Edge(self.vertices[(i + 1, j + 1)], match_points(A[i], B[j])))
+
+
 
 
 a = 'CCTGCTGCAGGATGTGCCG'
 b = 'GATGTGCAGCCTGTAGG' 
 
-print(build_match_matrix(a,b)) 
+print(Graph(a, b))
+
 
 
